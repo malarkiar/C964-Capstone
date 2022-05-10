2b37850d099b77996097c26b295a394fab507c43
tyanc3 <tyler.perkes@taxbit.com>
2022-05-10 11:53:52 -0600

    init commit 2

--- src/app.py
+++ src/app.py
@@ -1,3 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import model
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/sub", methods=["POST"])
+def submit():
+    # HTML -> .py
+    if request.method == "POST":
+        name = request.form["username"]
+    # .py -> HTML
+    return render_template("submit.html", n = name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/model.py
@@ -0,0 +1,11 @@
+# import pandas as pd
+# import numpy as np
+# import matplotlib as plt
+# from sklearn.linear_model import LinearRegression
+#
+#
+# def marks_prediction(marks):
+#     model = LinearRegression()
+#     return model.predict()
+#
+# marks_prediction()
