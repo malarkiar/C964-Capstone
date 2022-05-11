04af16a1ca6241d668183db098518a64a8dc8397
tyanc3 <tyler.perkes@taxbit.com>
2022-05-10 20:40:18 -0600

    added basic ui

--- app/functions.py
+++ app/functions.py
@@ -1,44 +1,21 @@
-def getCorrelatedAssets(symbol):
-    if symbol == 'BTC':
-        ethObj = {
-            "label": "ETH",
-            "correlation_score": .920,
-            "graph_image": "eth_btc.jpeg",
-            "graph_width": 600,
-            "graph_height": 200,
-            "graph_alt": "ETH/BTC Correlation"
+def getCorrelatedAssets():
+    return {
+        "btc": {
+            "eth": {
+                    "rAsset": "ETH",
+                    "cRatio": 0.44,
+                    "eReturn%": None,
+                    "eReturn": None,
+                    "eReturn%Delta": None,
+                    "eReturnDelta": None
+            }
         }
-
-        ltcObj = {
-            "label": "LTC",
-            "correlation_score": .871,
-            "graph_image": "ltc_btc.jpeg",
-            "graph_width": 600,
-            "graph_height": 200,
-            "graph_alt": "LTC/BTC Correlation"
-        }
-
-        avaxObj = {
-            "label": "AVAX",
-            "correlation_score": .774,
-            "graph_image": "avax_btc.jpeg",
-            "graph_width": 600,
-            "graph_height": 200,
-            "graph_alt": "AVAX/BTC Correlation"
-        }
-        return [ethObj, ltcObj, avaxObj]
+    }
 
 
-def getPricePrediction(symbol, period):
-    if symbol == 'ETH':
-        ethObj = {
-            "label": "ETH",
-            "weeks_future": period,
-            "future_price": 4165,
-            "graph_image": "eth_btc.jpeg",
-            "graph_width": 600,
-            "graph_height": 200,
-            "graph_alt": "ETH Future Price"
-        }
-
-        return ethObj
+def appendPredictions(asset_list, r_amount):
+    asset_list['btc'][0]['eReturn%'] = '+7%'
+    asset_list['btc'][0]['eReturn'] = '+201'
+    asset_list['btc'][0]['eReturn%Delta'] = '-1%'
+    asset_list['btc'][0]['eReturnDelta'] = '-28'
+    return asset_list
--- app/main.py
+++ app/main.py
@@ -1,31 +1,15 @@
 from flask import Flask, render_template, request
-from app.functions import getCorrelatedAssets, getPricePrediction
+from app.functions import getCorrelatedAssets, appendPredictions
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
-
-
-@app.route("/correlated-assets/", methods=["POST"])
-def correlatedAssets():
-    # HTML -> .py
+    asset_list = getCorrelatedAssets()
     if request.method == 'POST':
-        crypto = request.form["crypto"]
-    # .py -> HTML
-    correlated_assets = getCorrelatedAssets(crypto)
-    return render_template("correlated-assets.html", c=crypto, list=correlated_assets)
+        r_amount = request.form["r_amount"]
+        asset_list = appendPredictions(asset_list, r_amount)
+    return render_template("index.html", list=asset_list)
 
 
-@app.route("/price-prediction/", methods=["POST"])
-def pricePrediction():
-    # HTML -> .py
-    if request.method == 'POST':
-        crypto = request.form["crypto"]
-        period = request.form["period"]
-    # .py -> HTML
-    price_prediction = getPricePrediction(crypto, period)
-    return render_template("price-prediction.html", c=crypto, t=period, p=price_prediction)
 
