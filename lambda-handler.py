c4317e868d02b9cda842e409bbcc4a002b9d67e1
tyanc3 <tyler.perkes@taxbit.com>
2022-05-14 23:08:26 -0600

    refactored to use lambda handler as main file v2

--- lambda-handler.py
+++ lambda-handler.py
@@ -1,37 +1,43 @@
 import json
 
+def get_results(asset, amount):
+    q_asset = get_most_correlated_asset(asset)
+    b_predict = predict_price(asset)
+    q_predict = predict_price(q_asset)
+    p_delta = get_prediction_delta(b_predict, q_predict)
 
-def get_correlations(asset):
-    # TODO
-    return True
+    return [{
+        "bAsset": asset,
+        "qAsset": q_asset.get("symbol"),
+        "bqCorrelation": q_asset.get("correlation"),
+        "amount": amount,
+        "bPredict": b_predict,
+        "qPredict": q_predict,
+        "predictDelta": p_delta
+    }]
 
 
-def get_predictions(asset, amount):
-    # TODO
-    return True
+def get_most_correlated_asset(asset):
+    return {
+        "symbol": "ETH",
+        "correlation": 0.88
+    }
 
 
-def get_result(asset, amount):
-    correlation_response = get_correlations(asset)
-    prediction_response = get_predictions(asset, amount)
-    return True
+def predict_price(asset):
+    price_prediction = 1000
+    return price_prediction
+
+
+def get_prediction_delta(b_predict, q_predict):
+    return (q_predict/b_predict - 1) * 100
 
 
 def handle(event, context):
-    response = {}
     asset = json.loads(event.get('body')).get('asset')
     amount = json.loads(event.get('body')).get('amount')
-    response = get_result(asset, amount)
+    response = get_results(asset, amount)
     return {
         "statusCode": 200,
         "body": json.dumps(response)
-        # "body": json.dumps([{
-        #     "bAsset": "BTC",
-        #     "qAsset": "ETH",
-        #     "bqCorrelation": "0.55",
-        #     "amount": "100",
-        #     "bPredict": "98",
-        #     "qPredict": "88",
-        #     "predictDelta": "10%"
-        # }])
     }
