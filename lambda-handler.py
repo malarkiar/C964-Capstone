0b9a1c9914543d60bcea5793312dfdfd1f7b352c
tyanc3 <tyler.perkes@taxbit.com>
2022-05-14 23:04:51 -0600

    refactored to use lambda handler as main file

--- lambda-handler.py
+++ lambda-handler.py
@@ -1,5 +1,37 @@
+import json
+
+
+def get_correlations(asset):
+    # TODO
+    return True
+
+
+def get_predictions(asset, amount):
+    # TODO
+    return True
+
+
+def get_result(asset, amount):
+    correlation_response = get_correlations(asset)
+    prediction_response = get_predictions(asset, amount)
+    return True
+
+
 def handle(event, context):
+    response = {}
+    asset = json.loads(event.get('body')).get('asset')
+    amount = json.loads(event.get('body')).get('amount')
+    response = get_result(asset, amount)
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        "statusCode": 200,
+        "body": json.dumps(response)
+        # "body": json.dumps([{
+        #     "bAsset": "BTC",
+        #     "qAsset": "ETH",
+        #     "bqCorrelation": "0.55",
+        #     "amount": "100",
+        #     "bPredict": "98",
+        #     "qPredict": "88",
+        #     "predictDelta": "10%"
+        # }])
     }
